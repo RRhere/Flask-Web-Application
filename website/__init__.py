d5dad51ac74f7df3f9bd861838836a1d92a7306b
Ragav R <109599960+RRhere@users.noreply.github.com>
2024-07-06 19:00:57 +0530

    Changed database creation to only once

--- website/__init__.py
+++ website/__init__.py
@@ -1,46 +1,39 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from os import path
 from flask_login import LoginManager
 from flask_mail import Mail
 
-db=SQLAlchemy()
-DB_NAME="temp1.db"
+db = SQLAlchemy()
 mail = Mail()
+DB_NAME = "temp1.db"
 
 def create_app():
-    app=Flask(__name__)
-    app.config['SECRET_KEY'] = '12345'
-    app.config['SQLALCHEMY_DATABASE_URI']=f'sqlite:///{DB_NAME}'
+    app = Flask(__name__)
     app.config.from_object('config')
-    db.init_app(app)    
-    mail.init_app(app)
-        
-    from .views import views
-    from .auth import auth
     
-    app.register_blueprint(views, url_prefix='/')
-    app.register_blueprint(auth, url_prefix='/')
+    db.init_app(app)
+    mail.init_app(app)
+
+    from .views import views as views_blueprint
+    from .auth import auth as auth_blueprint
+    
+    app.register_blueprint(views_blueprint, url_prefix='/')
+    app.register_blueprint(auth_blueprint, url_prefix='/')
     
     from .models import User
-    
-    with app.app_context():
-        db.create_all()
-    
-    create_database(app)
-    
-    login_manager=LoginManager()
-    login_manager.login_view='auth.login'
-    login_manager.init_app(app)
-    
-    @login_manager.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-    
-    return app
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        with app.app_context():
+    with app.app_context():
+        if not os.path.exists(os.path.join('website', DB_NAME)):
             db.create_all()
-        print('Created Database!')
+            print('Created Database!')
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        return User.query.get(int(user_id))
+
+    return app
